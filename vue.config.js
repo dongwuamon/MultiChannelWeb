ecff803d123e71a9e431c4278dba93896bafd4fe
wuao <wuao@oppo.com>
2021-03-23 14:10:37 +0800

    vue

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,11 @@
 const buildConfig = require('./buildConfig.json')
-console.log('------------')
-console.log(buildConfig.channel)
-console.log('------------')
-// const entry = buildConfig.version
-// console.log(entry)
-// module.exports = {
-//   outputDir: 'dist',
-//   pages: {
-//     index: {
-//       entry: 'src/entry/' + entry + '.js',
-//       template: 'public/index.html'
-//     }
-//   }
-// }
+const channel = buildConfig.channel
+module.exports = {
+  outputDir: 'dist',
+  pages: {
+    index: {
+      entry: 'src/entry/' + channel + '.js',
+      template: 'public/index.html'
+    }
+  }
+}
